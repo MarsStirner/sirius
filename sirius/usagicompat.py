a483dd720a02d12975c141a33ff39e3ddba90c78
Dmitry Paschenko <paschenko@bars-open.ru>
2016-10-14 12:26:41 +0300

    RIMIS-1556 Реализация кейса передачи пациентки для Тамбова вариация конфига

--- sirius/usagicompat.py
+++ sirius/usagicompat.py
@@ -15,12 +15,22 @@ class BIUsagiClient(TsukinoUsagiClient):
 
     def on_configuration(self, configuration):
         configuration['APP_VERSION'] = app_version
-        is_dev = get_debug_flag()
-        is_test = os.environ.get('TESTING') == '1'
-        if is_test:
-            env_conf_name = 'test'
+        is_debug = get_debug_flag()
+        is_server = configuration['SERVER']
+        is_autotest = os.environ.get('TESTING') == '1'
+        if is_debug:
+            if is_server:
+                if is_autotest:
+                    env_conf_name = 'test_autotest'
+                else:
+                    env_conf_name = 'test'
+            else:
+                if is_autotest:
+                    env_conf_name = 'dev_autotest'
+                else:
+                    env_conf_name = 'dev'
         else:
-            env_conf_name = 'dev' if is_dev else 'prod'
+            env_conf_name = 'prod'
 
         env_conf = configuration.get(env_conf_name, {})
         configuration.update(env_conf)
