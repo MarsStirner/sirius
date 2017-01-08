b9f5f89024d3c639aed3df375156c08ffc5cb657
Dmitry Paschenko <paschenko@bars-open.ru>
2017-01-08 19:24:36 +0300

    Для тестов ускоряем обновление данных

--- sirius/celery_schedule.py
+++ sirius/celery_schedule.py
@@ -4,6 +4,6 @@ import datetime
 CELERYBEAT_SCHEDULE = {
     'schedule-every-five-minutes': {
         'task': 'sirius.lib.celery_tasks.scheduler_task',
-        'schedule': datetime.timedelta(minutes=3),
+        'schedule': datetime.timedelta(minutes=1),
     },
 }
